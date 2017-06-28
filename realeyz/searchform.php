615cbc40326492df3f616e847890412febf12a71
Matt Coffee <makoffee@antidata.net>
2017-06-28 15:02:11 +0000

    hold on to your butts

--- realeyz/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="form" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >
-    <input type="text" value="" name="s" id="s" class="form-control" placeholder="Blog Search" autocomplete="off" />
-    <button class="btn btn-style btn-search"><i class="fa fa-search"></i></button>
-</form>
